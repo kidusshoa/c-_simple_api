4a53b34c310d377fae816513c6aa6f8e755cc3c7
kidusshoa <captainkevin2008@gmail.com>
2025-03-12 15:02:53 +0300

    libraries and db

--- README.md
+++ README.md
@@ -8,3 +8,19 @@ C++ Notes App Server with Crow as the web framework and MySQL as the database.
 sudo apt update
 sudo apt install libmysqlcppconn-dev libboost-all-dev
 ```
+
+or
+
+```bash
+
+sudo apt install libasio-dev
+
+git clone https://github.com/CrowCpp/Crow.git
+cd Crow
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+
+```
--- server.cpp
+++ server.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+#include <crow_all>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/resultset.h>
 
+#define DB_HOST "tcp://localhost:3306"
+#define DB_USER "kidus"
+#define DB_PASS "kidus"
+#define DB_NAME "notes_app"
+
+sql::mysql::MySQL_Driver *driver;
+sql::Connection *con;
+
+void init_db() {
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect(DB_HOST, DB_USER, DB_PASS);
+    con->setSchema(DB_NAME);
+}
 int main(){
     cout<<"hello world"<<endl;
     return 0;
